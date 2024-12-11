a906412315de9de66602e9cbdca22ef6146a2014
andreeabrindea <andreeabrindea61@gmail.com>
2024-12-11 19:45:46 +0200

    add optimized method

--- LeetCode/LongestPalindrome.cs
+++ LeetCode/LongestPalindrome.cs
@@ -13,9 +13,55 @@ public class LongestPalindrome
     // Use string.Concat for IEnumerable<char> to string conversion
     // Ask questions: e.g: how should be the output in case of no palindromes? empty string, null, the actual string?
     // Time Limit Exceeded on Leetcode :(
+    // LINQ is not designed for tasks that involve updating variables
+    
+    
     public string FindLongestPalindrome(string s)
     {
-        ArgumentNullException.ThrowIfNullOrEmpty(s);
+        ArgumentException.ThrowIfNullOrEmpty(s);
+        if (s.Length < 2)
+        {
+            return s;
+        }
+
+        int maximumLength = 1;
+        int start = 0;
+        
+        for (int i = 0; i < s.Length; i++)
+        {
+            for (int j = i; j < s.Length; j++)
+            {
+                if (IsPalindrome(s, i, j) && (j - i + 1 > maximumLength))
+                {
+                    maximumLength = j - i + 1;
+                    start = i;
+                }
+            }
+        }
+
+        return s.Substring(start, maximumLength);
+    }
+
+    private bool IsPalindrome(string s, int start, int end)
+    {
+        int left = start;
+        int right = end;
+        while (left < right)
+        {
+            if (s[left] != s[right])
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+    public string FindLongestPalindromePoorPerformance(string s)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(s);
         if (s.Length < 2)
         {
             return s;
