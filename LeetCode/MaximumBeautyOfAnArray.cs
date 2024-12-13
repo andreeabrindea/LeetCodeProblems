0e55a10f08274f4d46cfd2643e7c165183ecdd39
andreeabrindea <andreeabrindea61@gmail.com>
2024-12-13 15:16:56 +0200

    add MaximumBeautyOfAnArray skeleton implementation

---
+++ LeetCode/MaximumBeautyOfAnArray.cs
@@ -0,0 +1,52 @@
+namespace Problems;
+
+public class MaximumBeautyOfAnArray
+{
+    
+    // 2779. Maximum Beauty of an Array After Applying Operation.
+    
+    // Steps:
+    // 1. Find the maximum number of equal elements.
+    // 2. Add k to each element?
+    // 3. Verify maximum no of equal elements
+    public int MaximumBeauty(int[] nums, int k)
+    {
+        ArgumentNullException.ThrowIfNull(nums);
+        Enumerable.Range(0, (int)Math.Pow(2, nums.Length))
+            .Select(p => p);
+        return k;
+    }
+
+    private int GetNumberOfEqualElements(int[] nums) => nums.Length - nums.Distinct().Count() + 1;
+
+    [Fact]
+    public void MaximumBeauty_InputIsNull()
+    {
+        int[] nums = null;
+        Assert.Throws<ArgumentNullException>(() => MaximumBeauty(nums, 0));
+    }
+
+    [Fact]
+    public void GetNumberOfEqualElements_InputHasSeveralEqualNumbers()
+    {
+        Assert.Equal(4, GetNumberOfEqualElements(new []{3, 3, 1, 2, 3, 3}));
+    }
+
+    [Fact]
+    public void MaximumBeauty_InputHasOneElement()
+    {
+        Assert.Equal(1, MaximumBeauty(new [] { 1 }, 2));
+    }
+
+    [Fact]
+    public void MaximumBeauty_InputHasSeveralEqualElements()
+    {
+        Assert.Equal(3, MaximumBeauty(new [] { 2, 3, 2, 2}, 5));
+    }
+
+    [Fact]
+    public void MaximumBeauty_InputHasNoEqualElements()
+    {
+        Assert.Equal(3, MaximumBeauty(new []{4,6,1,2}, 2));
+    }
+}
